0cb47a11d17d8244024fea66d075acd5358b35c5
evillt <hujiehao@deepexi.com>
2019-12-16 11:47:05 +0800

    feat: fallback

--- run.js
+++ run.js
@@ -1,7 +1,14 @@
+#!/usr/bin/env node
+
 const Script = require('./Script')
 const parseArgs = require('./utils/parseArgs')
 
 const args = parseArgs(process.argv.slice(2))
+
+if (!args.get('component')) {
+  console.error('Missing param: `component`')
+  process.exit()
+}
 
 const script = new Script({
   component: args.get('component')
